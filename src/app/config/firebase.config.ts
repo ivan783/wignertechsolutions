7c8ea67b40c72a60edffbb6c92df668f1e78d767
Ivan Flores <developflores@gmail.com>
2026-03-29 14:46:56 -0400

    cambios menores

--- src/app/config/firebase.config.ts
+++ src/app/config/firebase.config.ts
@@ -1,10 +1,20 @@
 // ⚠️ IMPORTANTE: Reemplaza estos valores con los de tu proyecto Firebase
 // Ve a https://console.firebase.google.com → Tu Proyecto → Configuración → Aplicaciones web
-export const firebaseConfig = {
+/* export const firebaseConfig = {
   apiKey: 'AIzaSyBsQqgPrpZ3HR7xhlKf42nWRXikYTlo1G0',
   authDomain: 'tu-proyecto.firebaseapp.com',
   projectId: 'floxpress-e468d',
   storageBucket: 'floxpress-e468d.firebasestorage.app',
   messagingSenderId: '741350704148',
   appId: 't1:741350704148:android:dce0210df5f267be82d301'
+}; */
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6OVz0jkX1wP3Mo-gRG8HNFWNI5VSjiGo",
+  authDomain: "wynova-1038e.firebaseapp.com",
+  projectId: "wynova-1038e",
+  storageBucket: "wynova-1038e.firebasestorage.app",
+  messagingSenderId: "148252954031",
+  appId: "1:148252954031:web:05479375e1752753b3735a",
+  measurementId: "G-C8HWSCGGJB"
 };
